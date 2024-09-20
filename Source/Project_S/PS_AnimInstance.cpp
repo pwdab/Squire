c82b1c84b4d7aae8742464453b5d367337c731e7
최연구 <augustinodot@naver.com>
2024-09-20 16:08:20 +0900

    AttackCodeAdd

--- Source/Project_S/PS_AnimInstance.cpp
+++ Source/Project_S/PS_AnimInstance.cpp
@@ -1,9 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "PS_AnimInstance.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/PawnMovementComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "PS_AnimInstance.h"
+#include "PS_Character.h"
 
 UPS_AnimInstance::UPS_AnimInstance()
 {
@@ -25,10 +26,21 @@ void UPS_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 	{
 		CurrentPawnSpeed = FVector2D(Pawn->GetVelocity().X, Pawn->GetVelocity().Y);
 		auto Character = Cast<ACharacter>(Pawn);
+		auto PS_Character = Cast<APS_Character>(Character);
 		if (Character)
 		{
 			IsInAir = Character->GetMovementComponent()->IsFalling();
 		}
+		
+		if (PS_Character->bIsAttacking)  // bIsAttacking은 가상의 변수로, 실제로는 캐릭터 클래스의 상태를 확인하는 로직이 필요
+		{
+			IsAttacking = true;
+		}
+		else
+		{
+			IsAttacking = false;
+		}
+
 	}
 }
 
--- Source/Project_S/PS_Character.h
+++ Source/Project_S/PS_Character.h
@@ -38,13 +38,16 @@ class PROJECT_S_API APS_Character : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class UInputAction> JumpAction;
 
+	// 공격 액션 추가
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> AttackAction;
 
 public:
-	// Sets default values for this character's properties
 	APS_Character();
+	// Attack 상태 변수
+	bool bIsAttacking;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	void Move(const struct FInputActionValue& Value);
@@ -55,16 +58,29 @@ protected:
 	void JumpStart(const FInputActionValue& Value);
 	void JumpEnd(const FInputActionValue& Value);
 
+	// 공격 함수
+	void AttackStart(const FInputActionValue& Value);
+
+	// Sprint Server functions
 	UFUNCTION(Server, Reliable)
 	void SprintStart_Server();
+
 	UFUNCTION(Server, Reliable)
 	void SprintEnd_Server();
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	// Trace에 사용할 변수
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	float AttackRange = 200.0f;
 
-	// Called to bind functionality to input
+	// 공격이 끝난 후 호출되는 함수
+	void EndAttack();
+
+	// 공격 판정이 유지되는 시간
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	float AttackDuration = 0.5f;
+
+public:
+	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
