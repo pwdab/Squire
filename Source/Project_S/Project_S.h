4b856a91ac772917bc4970d6c742ae2f04448526
pwdab <soundno07@naver.com>
2024-10-05 22:44:03 +0900

    커스텀 로깅 추가
    PS_LOG_CALLINFO
    PS_LOG_S
    PS_LOG
    PS_CHECK
    추가

--- Source/Project_S/Project_S.h
+++ Source/Project_S/Project_S.h
@@ -9,8 +9,9 @@ DECLARE_LOG_CATEGORY_EXTERN(Project_S, Log, All);
 
 // FUNCTION 이름과 LINE을 출력
 #define PS_LOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
-// Verbosity()를 인자로 받아 Verbosity에 맞는 PS_LOG_CALLINFO를 출력
+// Verbosity(로깅 수준 : Log, Warning, Error, ... )를 인자로 받아 Verbosity에 맞는 PS_LOG_CALLINFO를 출력
 #define PS_LOG_S(Verbosity) UE_LOG(Project_S, Verbosity, TEXT("%s"), *PS_LOG_CALLINFO)
-//
+//Verbosity를 인자로 받아 Verbosity에 맞는 PS_LOG_CALLINFO에 더불어 Format도 출력
 #define PS_LOG(Verbosity, Format, ...) UE_LOG(Project_S, Verbosity, TEXT("%s"), *PS_LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
-#define PS_CHECK(Expr, ...) { if (!(Expr)) { }}
+// Expr이 False이면 Error를 출력
+#define PS_CHECK(Expr, ...) { if (!(Expr)) { PS_LOG(Error, TEXT("ASSERTION : %s"), TEXT("'"#Expr"'")); return __VA_ARGS__; } }
