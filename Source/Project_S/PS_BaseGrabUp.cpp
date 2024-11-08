bcc248ae691bf911b3f4533f195cff907a2189ee
pwdab <soundno07@naver.com>
2024-11-08 23:39:17 +0900

    GrabUp

--- Source/Project_S/PS_BaseGrabUp.cpp
+++ Source/Project_S/PS_BaseGrabUp.cpp
@@ -11,29 +11,32 @@ APS_BaseGrabUp::APS_BaseGrabUp()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	/*
 	// Setup Capsule Component
 	CapsuleCollision = CreateDefaultSubobject<UCapsuleComponent>("Collision");
 	RootComponent = CapsuleCollision;
+	//CapsuleCollision->SetupAttachment(RootComponent);
+	CapsuleCollision->InitCapsuleSize(55.0f, 55.0f);
 	CapsuleCollision->SetGenerateOverlapEvents(true);
 	CapsuleCollision->bHiddenInGame = false;
-	*/
+	CapsuleCollision->SetCollisionProfileName(TEXT("Grabable"));
+	CapsuleCollision->SetSimulatePhysics(true);
+	CapsuleCollision->SetEnableGravity(true);
 
 	// Setup Mesh Component
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
 	//Mesh->SetupAttachment(CapsuleCollision);
 	Mesh->SetupAttachment(RootComponent);
 	//Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	GetMesh()->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
+	Mesh->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
 
 	bIsActive = false;
+	Owner = nullptr;
 }
 
 // Called when the game starts or when spawned
 void APS_BaseGrabUp::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(Project_S, Log, TEXT("GrabUp Begin Play\n"));
 }
 
 // Called every frame
@@ -41,8 +44,13 @@ void APS_BaseGrabUp::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (bIsActive) {
-		SetActorLocation(GetOwner()->GetActorLocation() + GetOwner()->GetActorForwardVector() * 200.0f);
+	if (bIsActive && Owner) {
+		// 캐릭터 몸통 앞에 위치
+		//SetActorLocation(GetOwner()->GetActorLocation() + GetOwner()->GetActorForwardVector() * 200.0f);
+
+		// 캐릭터 시선 방향에 위치
+		FVector ViewVector = FVector(FMath::Cos(FMath::DegreesToRadians(Owner->GetControlRotation().Yaw)), FMath::Sin(FMath::DegreesToRadians(Owner->GetControlRotation().Yaw)), FMath::Tan(FMath::DegreesToRadians(Owner->GetControlRotation().Pitch)));
+		SetActorLocation(GetOwner()->GetActorLocation() + ViewVector * 250.0f + FVector(0.0f, 0.0f, 25.0f));
 	}
 }
 
@@ -51,14 +59,20 @@ void APS_BaseGrabUp::Grab_Implementation(class APS_Character* CharacterInstigato
 {
 	UE_LOG(Project_S, Log, TEXT("Grab\n"));
 	bIsActive = true;
+	CapsuleCollision->SetSimulatePhysics(false);
+	CapsuleCollision->SetEnableGravity(false);
 	SetOwner(CharacterInstigator);
+	Owner = CharacterInstigator;
 }
 
 void APS_BaseGrabUp::UnGrab_Implementation(class APS_Character* CharacterInstigator)
 {
 	UE_LOG(Project_S, Log, TEXT("UnGrab\n"));
 	bIsActive = false;
+	CapsuleCollision->SetSimulatePhysics(true);
+	CapsuleCollision->SetEnableGravity(true);
 	SetOwner(nullptr);
+	Owner = nullptr;
 }
 
 void APS_BaseGrabUp::Throw_Implementation(class APS_Character* CharacterInstigator)
