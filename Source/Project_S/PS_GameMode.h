6f59cdcbaff5dfe2d9dc502204a70beeb0a41106
최연구 <augustinodot@naver.com>
2024-12-07 23:13:12 +0900

    정답 시간 조금 줄임
    나 힘들어

--- Source/Project_S/PS_GameMode.h
+++ Source/Project_S/PS_GameMode.h
@@ -98,7 +98,7 @@ private:
 	TArray<FString> ButtonWords;
 
 	// 타이머 시간
-	const uint8 GameStartWaitTime = 5;
+	const uint8 GameStartWaitTime = 2;
 	const uint8 SelectionTime = 10;
 	const uint8 GameSessionTime = 45;
 
