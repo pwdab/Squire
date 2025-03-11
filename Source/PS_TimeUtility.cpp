4a291571698d906497a55c94f12cd9e38a5e9f9c
pwdab <soundno07@naver.com>
2025-03-12 02:18:49 +0900

    Revert "Squire 업데이트"
    This reverts commit 479cfddbbcfc2937b8a6cc5536831f208fc6b779.

--- Source/PS_TimeUtility.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PS_TimeUtility.h"
-
-FTimespan UPS_TimeUtility::ConvertSecondsToTimespan(float Seconds)
-{
-    return FTimespan::FromSeconds(Seconds);
-}
