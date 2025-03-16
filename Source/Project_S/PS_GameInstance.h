34dbec4a77497428d36703018ab249273ee3a0ba
pwdab <soundno07@naver.com>
2025-03-16 13:16:10 +0900

    git lfs 오류 수정 중 4

---
+++ Source/Project_S/PS_GameInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Project_S.h"
+#include "Engine/GameInstance.h"
+#include "PS_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_S_API UPS_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+	UPS_GameInstance();
+	
+public:
+	void SetMap(uint8 InMap);
+	void SetStage(uint8 InStage);
+	void SetLife(uint8 InLife);
+	void DeductLife();
+	void SetIsGameStart(bool NewBool);
+
+	uint8 GetMap() const;
+	uint8 GetStage() const;
+	uint8 GetLife() const;
+	bool IsGameStart() const;
+
+private:
+	uint8 Map;
+	uint8 Stage;
+	uint8 Life;
+	bool bIsGameStart;
+};
