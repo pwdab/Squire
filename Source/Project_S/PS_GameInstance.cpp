c85c1de2fad0b9d745f390786c923c98409d1517
pwdab <soundno07@naver.com>
2025-03-17 20:37:31 +0900

    Steam 연결 성공
    - Launch Game하면 Steam 오버레이 활성화 됨
    - Create Session 작동 안 함 > Steam API로 Create Session하도록 바꿔야 함

--- Source/Project_S/PS_GameInstance.cpp
+++ Source/Project_S/PS_GameInstance.cpp
@@ -10,6 +10,40 @@ UPS_GameInstance::UPS_GameInstance()
     Life = 3;
 }
 
+void UPS_GameInstance::Init()
+{
+    IOnlineSubsystem* OnlineSubsystemInterface = IOnlineSubsystem::Get();
+    if (OnlineSubsystemInterface)
+    {
+        SessionInterface = OnlineSubsystemInterface->GetSessionInterface();
+
+        //PS_LOG(Warning, TEXT("Online : %s, this : %s"), *OnlineSubsystemInterface->GetSubsystemName().ToString(), *GetFirstLocalPlayerController()->GetName());
+
+        if (UWorld* World = GetWorld())
+        {
+            if (UGameInstance* GameInstance = World->GetGameInstance())
+            {
+                FString InstanceName = GameInstance->GetName();
+                UE_LOG(Project_S, Log, TEXT("Online : %s, GameInstance : %s"), *OnlineSubsystemInterface->GetSubsystemName().ToString(), *InstanceName);
+            }
+        }
+
+        if (GEngine)
+        {
+            GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, FString::Printf(TEXT("Online : %s"), *OnlineSubsystemInterface->GetSubsystemName().ToString()));
+        }
+        /*
+        if (SessionInterface.IsValid())
+        {
+            SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UPS_GameInstance::OnCreateSessionComplete);
+            SessionInterface->OnDestroySessionCompleteDelegates.AddUObject(this, &UPS_GameInstance::OnDestroySessionComplete);
+            SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UPS_GameInstance::OnFindSessionsComplete);
+            SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UPS_GameInstance::OnJoinSessionComplete);
+        }
+        */
+    }
+}
+
 void UPS_GameInstance::SetMap(uint8 InMap)
 {
     Map = InMap;
--- Source/Project_S/PS_GameInstance.h
+++ Source/Project_S/PS_GameInstance.h
@@ -4,6 +4,9 @@
 
 #include "Project_S.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
 #include "PS_GameInstance.generated.h"
 
 /**
@@ -17,6 +20,7 @@ class PROJECT_S_API UPS_GameInstance : public UGameInstance
 	UPS_GameInstance();
 	
 public:
+	virtual void Init() override;
 	void SetMap(uint8 InMap);
 	void SetStage(uint8 InStage);
 	void SetLife(uint8 InLife);
@@ -33,4 +37,7 @@ private:
 	uint8 Stage;
 	uint8 Life;
 	bool bIsGameStart;
+
+	IOnlineSessionPtr SessionInterface;
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
 };
